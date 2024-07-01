ed079cd0a80be03e87eafb987b62120acc5cf9e3
rmdayberry <dayberryrm@gmail.com>
2024-07-01 17:30:13 -0500

    Create Async/Await Project Deck of Cards
    Create Deck of Cards Project html, js, and css files  using async and await

---
+++ .history/Deck_of_Cards/Async-Await/deck_cards_20240701164636.js
@@ -0,0 +1,54 @@
+$(function () {
+  let baseURL = "https://deckofcardsapi.com/api/deck";
+
+  // 1. Draw a single card and log the value and suit
+  async function singleCard() {
+    let data = await $.getJSON(`${baseURL}/new/draw/`);
+    let { suit, value } = data.cards[0];
+    console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
+    $("#single-card").append(
+      `<p>${value.toLowerCase()} of ${suit.toLowerCase()}</p>`
+    );
+  }
+  singleCard();
+
+  // 2. Draw two cards from the same deck and log the values and suits
+  async function part2() {
+    let firstCardData = await $.getJSON(`${baseURL}/new/draw/`);
+    let deckId = firstCardData.deck_id;
+    let secondCardData = await $.getJSON(`${baseURL}/${deckId}/draw/`);
+    [firstCardData, secondCardData].forEach((cardData) => {
+      let { suit, value } = cardData.cards[0];
+      console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
+      $("#two-cards").append(
+        `<p>${value.toLowerCase()} of ${suit.toLowerCase()}</p>`
+      );
+    });
+  }
+  part2();
+
+  // 3. Draw cards from a deck until no cards are left
+  async function setup() {
+    let $btn = $("#draw-card");
+    let $cardArea = $("#card-area");
+
+    let deckData = await $.getJSON(`${baseURL}/new/shuffle/`);
+    $btn.show().on("click", async function () {
+      let cardData = await $.getJSON(`${baseURL}/${deckData.deck_id}/draw/`);
+      let cardSrc = cardData.cards[0].image;
+      let angle = Math.random() * 90 - 45;
+      let randomX = Math.random() * 40 - 20;
+      let randomY = Math.random() * 40 - 20;
+      $cardArea.append(
+        $("<img>", {
+          src: cardSrc,
+          css: {
+            transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`,
+          },
+        })
+      );
+      if (cardData.remaining === 0) $btn.remove();
+    });
+  }
+  setup();
+});
